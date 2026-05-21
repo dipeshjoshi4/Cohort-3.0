0c66839efc1700cb14193e39d4d8108599611dfe
dipeshjoshi4 <dipeshj2310@gmail.com>
2026-05-21 14:21:51 +0530

    js phas-1

---
+++ DSA-JS/script.js
@@ -0,0 +1,12 @@
+
+//!JS Essential
+
+//?Difference between let var and const
+
+//1.sum of 2 interger
+//2.relation between interger and string
+//3.sum and message
+    //- type coercion
+    //- Greet the User
+//4.Accept and print the Answer
+//5.Swap Two Variables via 3 Methods
--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,15 @@
-console.log("Home")
+
+//? prompt confirm
+
+
+// var a = prompt("are you sure?");
+// console.log(a)
+// console.log(typeof(a)) //?string return
+
+// var b = confirm("this isconfirm");
+// console.log(b)
+// console.log(typeof(b)) //?boolean return
+
+// var c = alert("are you sure?");
+// console.log(c)
+// console.log(typeof(c)) //?undefiend return
