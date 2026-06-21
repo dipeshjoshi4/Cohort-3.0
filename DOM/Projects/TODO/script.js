2ee14ede0ede2b8c384b36e73854889c8bc7e6c5
dipeshjoshi4 <dipeshj2310@gmail.com>
2026-06-21 15:55:58 +0530

    todo

---
+++ DOM/Projects/TODO/script.js
@@ -0,0 +1,24 @@
+const inp = document.querySelector("input");
+const btn = document.querySelector("#add");
+const todoBox = document.querySelector(".todo-list");
+
+btn.addEventListener("click", () => {
+  const value = inp.value;
+
+  if (value.trim() === "") return;
+
+  todoBox.innerHTML += ` <div class="li">
+                <h3>${value}</h3>
+                <div>
+                    <button class="btn edit">Edit</button>
+                    <button class="btn del">Delete</button>
+                </div>
+            </div>`;
+
+  inp.value = "";
+
+});
+
+
+//make this via createelement
+//complete edit and delete function
